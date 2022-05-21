ed4a1011f52cc4cf5a901bbf922fa73d4e28950d
Miguel Muscat <miguelmuscat93@gmail.com>
2022-05-21 11:55:12 +0200

    Revised table methods on `Atlas`

--- src/Atlas.php
+++ src/Atlas.php
@@ -17,19 +17,24 @@ class Atlas
         $this->tables = [];
     }
 
-    public function getTable(string $name): ?Table
-    {
-        return $this->tables[$name] ?? null;
-    }
-
-    public function addTable(string $name, ?Schema $schema = null): ?Table
-    {
-        return $this->tables[$name] = new Table($this->config, $name, $schema);
-    }
-
     public function table(string $name, ?Schema $schema = null): Table
     {
-        return $this->tables[$name] ?? $this->tables[$name] = new Table($this->config, $name, $schema);
+        if (!array_key_exists($name, $this->tables) || $schema !== null) {
+            $this->tables[$name] = new Table($this->config, $name, $schema);
+        }
+
+        return $this->tables[$name];
+    }
+
+    public function getConfig(): Config
+    {
+        return $this->config;
+    }
+
+    /** @return Table[] */
+    public function getTables(): array
+    {
+        return $this->tables;
     }
 
     public static function createDefault(): self
---
+++ tests/AtlasTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace RebelCode\Atlas\Test;
+
+use RebelCode\Atlas\Atlas;
+use PHPUnit\Framework\TestCase;
+use RebelCode\Atlas\Config;
+use RebelCode\Atlas\Schema;
+use RebelCode\Atlas\Table;
+
+class AtlasTest extends TestCase
+{
+    public function testConstructor()
+    {
+        $config = $this->createMock(Config::class);
+        $atlas = new Atlas($config);
+
+        $this->assertSame($config, $atlas->getConfig());
+        $this->assertCount(0, $atlas->getTables());
+    }
+
+    public function testDefault()
+    {
+        $atlas = Atlas::createDefault();
+
+        $this->assertEquals(Config::createDefault(), $atlas->getConfig());
+        $this->assertCount(0, $atlas->getTables());
+    }
+
+    public function testTable()
+    {
+        $config = $this->createMock(Config::class);
+        $atlas = new Atlas($config);
+
+        $table = $atlas->table('test');
+
+        $this->assertEquals('test', $table->getName());
+        $this->assertNull($table->getSchema());
+    }
+
+    public function testTableWithSchema()
+    {
+        $config = $this->createMock(Config::class);
+        $atlas = new Atlas($config);
+
+        $schema = $this->createMock(Schema::class);
+        $table = $atlas->table('test', $schema);
+
+        $this->assertEquals('test', $table->getName());
+        $this->assertSame($schema, $table->getSchema());
+    }
+
+    public function testTableCache()
+    {
+        $config = $this->createMock(Config::class);
+        $atlas = new Atlas($config);
+
+        $table1 = $atlas->table('test');
+        $table2 = $atlas->table('test');
+
+        $this->assertSame($table1, $table2);
+    }
+
+    public function testTableCacheWithSchema()
+    {
+        $config = $this->createMock(Config::class);
+        $atlas = new Atlas($config);
+
+        $schema = $this->createMock(Schema::class);
+        $table1 = $atlas->table('test');
+        $table2 = $atlas->table('test', $schema);
+
+        $this->assertNull($table1->getSchema());
+        $this->assertSame($schema, $table2->getSchema());
+    }
+}
