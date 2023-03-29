7e299016dd98b083574ec94be10ed9fcafec70cf
Miguel Muscat <miguelmuscat93@gmail.com>
2023-03-29 14:29:07 +0200

    Annotate functions with `@psalm-pure`

--- inc/functions.php
+++ inc/functions.php
@@ -11,6 +11,7 @@ use RebelCode\Atlas\Expression\Term;
 /**
  * Creates a new column term.
  *
+ * @psalm-pure
  * @param string|ColumnTerm|Table|TableRef $arg1 The column name or term if only 1 arg is given. Otherwise, the table
  *                                               name, instance, or reference.
  * @param string|null $arg2 The column name if the 1st arg is a table name, instance, or reference.
@@ -40,6 +41,7 @@ function col($arg1, ?string $arg2 = null): ColumnTerm
 /**
  * Creates a data source for a table. This is an alias for the {@link TableRef} constructor.
  *
+ * @psalm-pure
  * @param string $name The table name.
  * @param string|null $alias Optional alias.
  * @return TableRef The created data source.
@@ -54,6 +56,7 @@ function table(string $name, ?string $alias = null): TableRef
  *
  * Note that this creates a join with an empty table name. Be sure to call {@link Join::with()} to set the table name.
  *
+ * @psalm-pure
  * @param string $type The join type.
  * @param DataSource|null $with The data source to join with.
  * @param ExprInterface|null $on The condition to join on.
@@ -67,6 +70,7 @@ function using(string $type, ?DataSource $with = null, ?ExprInterface $on = null
 /**
  * Creates an ascending order instance for a column.
  *
+ * @psalm-pure
  * @param string $column The column name.
  * @return Order|Group The order instance.
  */
@@ -78,6 +82,7 @@ function asc(string $column): Order
 /**
  * Creates a descending order instance for a column.
  *
+ * @psalm-pure
  * @param string $column The column name.
  * @return Order|Group The order instance.
  */
@@ -93,6 +98,7 @@ function desc(string $column): Order
  * three arguments, this function creates a binary expression using the first argument as the left-hand side, the second
  * argument as the operator, and the third argument as the right-hand side.
  *
+ * @psalm-pure
  * @param mixed $value The value to create the term from.
  * @param string|null $operator Optional operator to create a binary expression.
  * @param mixed|null $value2 Optional second value to create a binary expression.
@@ -114,6 +120,7 @@ function expr($value, ?string $operator = null, $value2 = null): ExprInterface
  * Creates a boolean NOT unary expression. This is an alias for calling the {@link ExprInterface::not()} method on the
  * passed argument.
  *
+ * @psalm-pure
  * @param ExprInterface $expr The expression to negate.
  * @return ExprInterface The created expression.
  */
@@ -126,6 +133,7 @@ function not(ExprInterface $expr): ExprInterface
  * Creates a number negation unary expression. This is an alias for calling the {@link ExprInterface::not()} method on
  * the passed argument.
  *
+ * @psalm-pure
  * @param ExprInterface $expr The expression to negate.
  * @return ExprInterface The created expression.
  */
@@ -137,6 +145,7 @@ function neg(ExprInterface $expr): ExprInterface
 /**
  * Creates a distinct column term. This is an alias for calling the {@link ColumnTerm::distinct()} method on the column.
  *
+ * @psalm-pure
  * @param ColumnTerm $col The column term.
  * @return ColumnTerm The distinct column term.
  */
